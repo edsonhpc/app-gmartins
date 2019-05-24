beecb22b655ec150ba603e8c7377fb64ffa883f0
Edson Cavalcanti <edson.horacavalcanti@gmail.com>
2019-05-24 19:16:15 -0300

    adding sign-in base

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { Routes } from "@angular/router";
+
+// Import Pages
+import { SignInComponent } from './sign-in/sign-in.component';
+
+const routes: Routes = [
+    { path: "", redirectTo: "/sign-in", pathMatch: "full" },
+    { path: "sign-in", component: SignInComponent}
+];
+
+@NgModule({
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sign-in',
+    moduleId: module.id,
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.css']
+})
+
+export class SignInComponent {}
